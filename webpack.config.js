0627cacb17214c4e8708342e0267b9ce37beb97e
Vicente Albert <vicente.albert.lopez@externos.gloval.es>
2022-03-10 18:11:12 +0100

    added different configuration for production and development environments and remove old builds before create new

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 const rulesForStyles = {
   test: /\.css$/,
@@ -19,19 +20,25 @@ const ruleForReact = {
   }
 };
 
-const rules = [ ruleForReact, rulesForStyles ];
+const rules = [ruleForReact, rulesForStyles];
 
-module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'build'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: "src/index.html"})
-  ],
-  module: { rules },
-  devServer: {
-    open: true,
-    port: 3000,
-  },
-  devtool: 'source-map'
+module.exports = (env, argv) => {
+  const {mode} = argv;
+  const isProd = mode === 'production';
+  return {
+    output: {
+      filename: isProd ? "mytheresa.[name].[contenthash].js" : "main.js",
+      path: path.resolve(__dirname, 'build'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: "src/index.html"}),
+      new CleanWebpackPlugin()
+    ],
+    module: {rules},
+    devServer: {
+      open: true,
+      port: 3000,
+    },
+    devtool: 'source-map'
+  }
 }
